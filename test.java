b43095a08118ba192985a4c6491cae40ca1c51b1
Benny Iko <benny.iko.it@gmail.com>
2022-02-17 18:46:44 -0500

    Changes to be committed: 	new file:   test.java     On branch master 	Changes: 	N/A 	Issues: 	N/A

---
+++ test.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class test here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class test
+{
+    public int cool =3;
+    public test(int cool){
+        cool = cooler;
+        System.out.println(cool);
+    }
+    
+    
+    public static void main(String [] args){
+        
+        test fort = new test(5);
+        fort.cool =3;
+       
+        
+        
+    }
+    
+    public static int cooler = 0;
+}
